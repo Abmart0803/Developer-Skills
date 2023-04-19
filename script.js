cce72e4f4137a4dca0367a2403af012f138de130
Abiola Martins <hsemartins@gmail.com>
2023-04-19 07:38:19 +0300

    Do the challenge work.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable func-names */
 
+const { Http2ServerRequest } = require('http2');
+
 const calcAge = (birthYear) => 2023 - birthYear;
 
 console.log(calcAge(1980));
@@ -146,3 +148,44 @@ const calcTempAmplitudeBug = function (t1, t2) {
 };
 const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeBug);
+
+// Developer Skills & Editor Setup
+// Coding Challenge #1
+// Given an array of forecasted maximum temperatures, the thermometer displays a
+// string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+// days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+// Your tasks:
+// 1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+// string like the above to the console. Try it with both test datasets.
+// 2. Use the problem-solving framework: Understand the problem and break it up
+// into sub-problems!
+// Test data:
+// § Data 1: [17, 21, 23]
+// § Data 2: [12, 5, -5, 0, 4]
+// GOOD LUCK 😀
+
+// 1. Understanding the problems
+// How to transform array to string with 0C.
+// What is the x days? Answer: index + 1
+
+// 2. Breaking the problem into sub problem
+// Transform array into string
+// Transfor each element to string with 0C
+//Strings needs to contain day (index + 1).
+// Add ... between element and start and end of string.
+
+//Solution
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ...`;
+  }
+  console.log('...' + str);
+};
+printForecast(data1);
+
+printForecast(data2);
